7cf952e7df082060baf2de3be224d5ac75d34519
FAWAD AHMAD <94217234+fawad7998@users.noreply.github.com>
2023-10-30 16:24:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,141 @@
-import React, { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
-import NotesList from "./components/NotesList";
-import Search from "./components/Search";
-import Header from "./components/Header";
+import React, { useState } from "react";
+import "./App.css";
 
-const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "The Product is awesome!!",
-      date: "15/04/2021",
-    },
-    {
-      id: nanoid(),
-      text: "Value for money",
-      date: "21/04/2021",
-    },
-    {
-      id: nanoid(),
-      text: "Room for improvement!",
-      date: "28/04/2021",
-    },
-    {
-      id: nanoid(),
-      text: "didn't meet the expectations!",
-      date: "30/04/2021",
-    },
-  ]);
+function App() {
+  const [reviews, setReviews] = useState([]);
+  const [letterCount, setLetterCount] = useState(0);
+  const maxLetterLimit = 400;
 
-  const [searchText, setSearchText] = useState("");
+  const handleInputChange = (e) => {
+    const inputText = e.target.value;
+    setLetterCount(inputText.length);
 
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
-
-    if (savedNotes) {
-      setNotes(savedNotes);
+    if (inputText.length > maxLetterLimit) {
+      document.getElementById("letter-limit").innerText = "You have reached the limit";
+    } else {
+      document.getElementById("letter-limit").innerText = `Letter: ${inputText.length}`;
     }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
-  }, [notes]);
-
-  const addNote = (text) => {
-    const date = new Date();
-    const newNote = {
-      id: nanoid(),
-      text: text,
-      date: date.toLocaleDateString(),
-    };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
   };
 
-  const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
+  const handleAddReview = () => {
+    const inputText = document.getElementById("user-input").value;
+
+    if (inputText.trim() === "") {
+      alert("Please enter a review.");
+      return;
+    }
+
+    if (letterCount > maxLetterLimit) {
+      alert("You have reached the letter limit.");
+      return;
+    }
+
+    const review = {
+      text: inputText,
+      date: new Date().toLocaleDateString(),
+    };
+
+    setReviews([...reviews, review]);
+
+    document.getElementById("user-input").value = "";
+    setLetterCount(0);
+    document.getElementById("letter-limit").innerText = "Letter: 0";
+  };
+
+  const handlePublish = (index) => {
+    const review = reviews[index];
+    const publishedReview = (
+      <div key={index} className="userReview w-1/3 m-6 bg-green-200 h-[300px]">
+        <div className="review-info">
+          <div className="flex flex-col items-center justify-between space-y-48">
+            <p className="h-[40px] w-full bg-green-200 outline-none px-4 ">
+              {review.text}
+            </p>
+            <div className="extra-info flex w-full justify-between px-8 p-4">
+              <h1 id="letter-limit" className="bg-green-400 py-1 px-5 rounded text-sm">
+                Letter: {review.text.length}
+              </h1>
+              <h1 id="Review Date" className="bg-green-400 py-1 px-5 rounded text-sm">
+                {review.date}
+              </h1>
+              <button
+                id="delete"
+                className="bg-green-400 rounded py-1 px-3 text-sm"
+                onClick={() => handleDelete(index)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+    setReviews(reviews.filter((_, i) => i !== index));
+    document.getElementById("published-reviews").appendChild(publishedReview);
+  };
+
+  const handleDelete = (index) => {
+    setReviews(reviews.filter((_, i) => i !== index));
   };
 
   return (
-    <div className={`${darkMode && "dark-mode"}`}>
-      <div className="container">
-        <Header handleToggleDarkMode={setDarkMode} />
-        <Search handleSearchNote={setSearchText} />
-        <NotesList
-          notes={notes.filter((note) =>
-            note.text.toLowerCase().includes(searchText)
-          )}
-          handleAddNote={addNote}
-          handleDeleteNote={deleteNote}
-        />
-      </div>
-    </div>
+    <>
+      <main className="main">
+        <h1 className="text-2xl text-black text-center">Review App</h1>
+        <div className="search w-full h-16">
+          <div className="flex flex-row items-center justify-center p-4">
+            <div className="flex w-full">
+              <input
+                type="text"
+                placeholder="Enter Review!"
+                className="outline-none bg-gray-100 px-3 py-1 rounded w-full"
+                id="user-input"
+                onChange={handleInputChange}
+              />
+              
+            </div>
+          </div>
+        </div>
+        <button
+          className="text-lg text-black ml-[570px] bg-green-400 hover:bg-green-200 py-1 px-4 rounded"
+          id="Add-review"
+          onClick={handleAddReview}
+        >
+          New Review
+        </button>
+
+        <div className="reviews w-full flex items-center flex-wrap justify-evenly" id="published-reviews">
+          {reviews.map((review, index) => (
+            <div key={index} className="userReview w-1/3 m-6 bg-green-200 h-[300px]">
+              <div className="review-info">
+                <div className="flex flex-col items-center justify-between space-y-48">
+                  <p className="h-[40px] w-full bg-green-200 outline-none px-4 ">
+                    {review.text}
+                  </p>
+                  <div className="extra-info flex w-full justify-between px-8 p-4">
+                    <h1 id="letter-limit" className="bg-green-400 py-1 px-5 rounded text-sm">
+                      Letter: {review.text.length}
+                    </h1>
+                    <h1 id="Review Date" className="bg-green-400 py-1 px-5 rounded text-sm">
+                      {review.date}
+                    </h1>
+                    <button
+                      id="delete"
+                      className="bg-green-400 rounded py-1 px-3 text-sm"
+                      onClick={() => handleDelete(index)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   );
-};
+}
 
 export default App;
